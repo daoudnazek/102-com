81016d62760ded1e9433174fe4e0c4c78a9de6ff
daoudnazek <dawood_nazek_92@hotmail.com>
2020-05-03 03:22:54 +0300

    add loops

--- js/myjava.js
+++ js/myjava.js
@@ -1,27 +1,44 @@
 'use strict';
-var name = prompt('please enter your name')
 
-document.getElementById("name").innerHTML = 'Welcome to our website ' + name + "!";
+function userName() {
+    var name = prompt('please enter your name')
+    var noOfName = '';
+    var nameNum = prompt('how many times would you like your name to be printed? ')
+    for (var i = 0; i < nameNum; i++) {
+        noOfName = noOfName + ' ' + name;
+    }
 
 
-var interest = confirm('Are you interested in Solar Energy?')
-var x;
-
-if (interest == true){
-    x = 'This is the right place for you'
-} else {
-    x = 'we hope you changed your mind after visiting our website'
+    return noOfName;
 }
-document.getElementById("answer").innerHTML = x
+
+
+function userInterest() {
+    var interest = confirm('Are you interested in Solar Energy?')
+    var answer = '';
+    if (interest == true) {
+        answer = 'This is the right place for you'
+    } else {
+        answer = 'we hope you changed your mind after visiting our website'
+    }
+    return answer;
+    //document.getElementById("answer").innerHTML = x
+}
+
+
 
 
 var backcolor = prompt('what is your favourite color? black, red, or blue ')
-if (backcolor == "black"){
-    document.getElementById("back").style.backgroundColor = 'black'
-} else if (backcolor == "red"){
-    document.getElementById("back").style.backgroundColor = 'red'
-} else if (backcolor == "blue"){
-    document.getElementById("back").style.backgroundColor = 'blue'
-} else {
-    document.getElementById("back").style.backgroundColor = 'green'
+
+while (backcolor !== 'black' && backcolor !== 'red' && backcolor !== 'blue') {
+    backcolor = prompt('what is your favourite color? black, red, or blue ')
 }
+
+if (backcolor == "black") {
+    document.getElementById("back").style.backgroundColor = 'black'
+} else if (backcolor == "red") {
+    document.getElementById("back").style.backgroundColor = 'red'
+} else if (backcolor == "blue") {
+    document.getElementById("back").style.backgroundColor = 'blue'
+}
+
